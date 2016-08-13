784331974ef9bb243fcef0a86e2758a58a039a67
Martin Groen <martin@martinjgroen.com>
2016-08-14 00:07:41 +1000

    Fix keychain to use just id_rsa for now

--- zsh/10-keychain.zsh
+++ zsh/10-keychain.zsh
@@ -3,6 +3,6 @@
 if [[ $SSH_CONNECTION == "" ]]; then
   which keychain &>/dev/null
   if [ $? -eq 0 ]; then
-    eval `keychain --inherit any --eval --agents ssh`
+    eval `keychain --inherit any --eval --quiet --agents ssh id_rsa`
   fi
 fi
