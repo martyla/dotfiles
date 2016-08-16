7900ca41e2c442ee7bd59569fdf28c586c492aa6
Martin Groen <martin@martinjgroen.com>
2016-08-16 23:59:45 +1000

    Add super basic git prompt (just branch name)

--- zsh/99-prompt.zsh
+++ zsh/99-prompt.zsh
@@ -8,7 +8,13 @@ function virtualenv_info {
     [ $VIRTUAL_ENV ] && echo '('`basename $VIRTUAL_ENV`')'
 }
 
-setopt promptsubst
-PROMPT='%{$fg_bold[red]%}%n%{$reset_color%} at %{$fg_bold[yellow]%}$(hostname)%{$reset_color%} in %{$fg_bold[green]%}${PWD/#$HOME/~}%{$reset_color%}
+function git_prompt() {
+  local ref="$(git rev-parse --symbolic-full-name --abbrev-ref HEAD 2>/dev/null)"
+  if [ -n "$ref" ]; then
+    echo " on %{$fg_bold[magenta]%}${ref#refs/heads/}%{$reset_color%}"
+  fi
+}
+
+PROMPT='%{$fg_bold[red]%}%n%{$reset_color%} at %{$fg_bold[yellow]%}$(hostname)%{$reset_color%} in %{$fg_bold[green]%}${PWD/#$HOME/~}%{$reset_color%}$(git_prompt)
 $(virtualenv_info)%(?,,%{${fg_bold[white]}%}[%?]%{$reset_color%} )$ '
 RPROMPT=''
