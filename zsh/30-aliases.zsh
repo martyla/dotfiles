1487f582cd8a83bfbdde0650dd5d5debcb15e2d4
Martin Groen <mgroen@freelancer.com>
2017-09-21 12:05:44 +1000

    Add gcalcli alias

--- zsh/30-aliases.zsh
+++ zsh/30-aliases.zsh
@@ -18,6 +18,8 @@ alias rip="curl http://icanhazip.com"
 # Reload shell (invoke as login shell)
 alias reload="exec $SHELL -l"
 
+alias cal='watch -c -n 180 gcalcli agenda today --military --defaultCalendar "mgroen@freelancer.com"'
+
 # Prefer nvim if it is installed
 if [[ -e "$(which nvim)" ]]; then
     alias vim="nvim"
