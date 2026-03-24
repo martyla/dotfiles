54c977a92a1e51b4f04615179d9ed5f93d2f8ff1
Martin Groen <martin@martinjgroen.com>
2026-03-24 22:47:32 +1100

    nvim: update vim-move to use alt modifier

--- dot_config/nvim/lua/plugins/move.lua
+++ dot_config/nvim/lua/plugins/move.lua
@@ -1,7 +1,7 @@
 return {
     'matze/vim-move',
     init = function()
-        vim.g.move_key_modifier = 'C'
-        vim.g.move_key_modifier_visualmode = 'C'
+        vim.g.move_key_modifier = 'A'
+        vim.g.move_key_modifier_visualmode = 'A'
     end
 }
